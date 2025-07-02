aee5010a46adc2be8b4e3e0876eae04c0aa3b9e3
Jonathan Maietta <jonathanmaietta@gmail.com>
2025-07-02 10:57:39 -0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDxHijS3cGbVq-mHFc5SfdgyqnbKUJarYQ",
+  authDomain: "ceorater-dacef.firebaseapp.com",
+  projectId: "ceorater-dacef",
+  storageBucket: "ceorater-dacef.firebasestorage.app",
+  messagingSenderId: "772315733470",
+  appId: "1:772315733470:web:990b30df47ba3fc8fc914f",
+  measurementId: "G-NRNZ6WZNE8"
+};
