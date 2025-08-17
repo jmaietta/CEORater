61c4e44336e0195cd251f197b6d6d53450b34ff0
Jonathan Maietta <jonathanmaietta@gmail.com>
2025-08-17 10:29:26 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
 const firebaseConfig = {
   apiKey: "AIzaSyDxHijS3cGbVq-mHFc5SfdgyqnbKUJarYQ",
-  authDomain: "ceorater-dacef.firebaseapp.com",
+  authDomain: "ceorater.com",
   projectId: "ceorater-dacef",
   storageBucket: "ceorater-dacef.firebasestorage.app",
   messagingSenderId: "772315733470",
   appId: "1:772315733470:web:990b30df47ba3fc8fc914f",
   measurementId: "G-NRNZ6WZNE8"
+
 };
